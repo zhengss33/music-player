6025d86902eb3e8a8249041d5b20fcbca10cbb7b
zhengss <zhengss332@gmail.com>
2018-04-20 14:57:16 +0800

    test

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,17 +1,16 @@
-// import jsonp from 'common/js/jsonp';
 import axios from 'axios';
+import { baseUrl } from './config';
 
 const debug = process.env.NODE_ENV !== 'production';
 
 export function getBanner() {
-  const url = debug ? '/api/banner' : 'https://zhengss33.github.io/music-player/api/banner';
+  const url = debug ? '/api/banner' : `${baseUrl}/api/banner`;
 
   return axios.get(url).then(res => Promise.resolve(res.data));
 }
 
 export function getDiscList() {
-  const url = debug ? '/api/top/playlist' : 'https://zhengss33.github.io/music-player/api/top/playlist';
-
+  const url = debug ? '/api/top/playlist' : `${baseUrl}/top/playlist`;
   return axios.get(url, {
     params: {
       limit: 10,
